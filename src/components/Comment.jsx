71a56d3a2c69a6db56f4013410fa9dcffdcd7f76
0xhelton <0xhelton@gmail.com>
2024-05-08 15:46:55 -0300

    Componente Comentários

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./Comment.module.css";
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+
+const Comment = () => {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/145160596?v=4" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Helton Muniz</strong>
+              <time title="11 de Maio às 08:13h" datetime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
